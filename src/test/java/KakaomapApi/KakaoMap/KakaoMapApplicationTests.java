4cf9defe1ad9dadea63a4edd988136e153f8ff8a
ulsandonghun <donghoon141@naver.com>
2023-05-16 01:37:23 +0900

    [fix] : 개발환경 세팅

---
+++ src/test/java/KakaomapApi/KakaoMap/KakaoMapApplicationTests.java
@@ -0,0 +1,13 @@
+package KakaomapApi.KakaoMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
